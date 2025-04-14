371e47337c47ae38803ef8324c088548b58d67df
josjaimes <joshuajaimes09@gmail.com>
2025-04-14 14:33:59 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.B, function () {
+    game.setLife(1)
+})
+basic.showString("Hello, young adventurer")
+let Pokemon = 0
+music.play(music.stringPlayable("A B C5 B A C5 A F ", 250), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("C D C E C D C D ", 250), music.PlaybackMode.UntilDone)
+basic.forever(function () {
+	
+})
